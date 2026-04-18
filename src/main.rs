faa15ff609bf673b8acc86600e295557470fd6ed
atesoro-dev <atesoro.dev@gmail.com>
2026-04-18 18:50:52 +0900

    シンプルな引数取得とファイルコピーを実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
-mod add; 
-fn main() {
-    println!("Hello, world!");
-    println!("2 + 3 = {}", add::add(2, 3));
+use std::env;
+use std::fs;
+use std::io;
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    dbg!(&args);
+    fs::copy(&args[1], &args[2])?;
+    Ok(())
 }
