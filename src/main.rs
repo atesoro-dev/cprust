0d59cbc121a13cc5650a2dac7709dfe13ffebf99
atesoro-dev <atesoro.dev@gmail.com>
2026-04-18 18:53:10 +0900

    2つのファイル名を変数に格納するように変更

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,9 @@ use std::io;
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
-    fs::copy(&args[1], &args[2])?;
+    
+    let src_file:&String = &args[1];
+    let dst_file:&String = &args[2];
+    fs::copy(src_file, dst_file)?;
     Ok(())
 }
