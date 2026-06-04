aa0b3d5cb4e151ced49f17f4ac698424ea6baabd
atesoro-dev <atesoro.dev@gmail.com>
2026-06-04 22:32:10 +0900

    add: パーサーを追加

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,55 @@ use std::env;
 use std::fs;
 use std::io;
 use std::path::Path;
+use std::path::PathBuf;
+
+fn parse_args(args: &[String]) -> (Vec<String>, Vec<PathBuf>) {
+    let mut options = Vec::new();
+    let mut paths = Vec::new();
+
+    let mut force_as_path = false;
+
+    for arg in args.iter().skip(1) {
+
+        if force_as_path {
+            paths.push(PathBuf::from(arg));
+            continue;
+        }
+
+        if arg == "--" {
+            force_as_path = true;
+        }
+        else if arg == "-" {
+            paths.push(PathBuf::from(arg));
+        }
+        else if arg.starts_with("--") {
+            options.push(arg.clone());
+        }
+        else if arg.starts_with('-') && arg.len() > 1 && !arg[1..].contains('-'){
+            for ch in arg.chars().skip(1) {
+                options.push(format!("-{}", ch));
+            }
+        }
+        else if arg.starts_with('-') {
+            options.push(arg.clone());
+        }
+        else {
+            paths.push(PathBuf::from(arg));
+        }
+    }
+
+    (options, paths)
+}
 
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     dbg!(&args);
+
+    let (options, paths) = parse_args(&args);
+
+    println!("Options: {:?}", options);
+    println!("Paths: {:?}", paths);
+    
     let dst_path: &Path = Path::new(&args[2]);
     if dst_path.is_dir() {
         println!("{} is a directory", dst_path.display());
